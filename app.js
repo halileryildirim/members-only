5d5d103acfb5853c1056d50afbb826a82ec595f2
halileryildirim <83421076+halileryildirim@users.noreply.github.com>
2024-06-03 03:45:04 +0300

    Add sign up form

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
+require("dotenv").config();
 var createError = require("http-errors");
 var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
-require("dotenv").config();
 
 const mongoose = require("mongoose"); // mongoose setup
 mongoose.set("strictQuery", false);
