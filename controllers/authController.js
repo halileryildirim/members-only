e53901ca7facbb3f4f13fa6593f8582c9a44b807
halileryildirim <83421076+halileryildirim@users.noreply.github.com>
2024-06-06 03:45:12 +0300

    Add login view

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,3 +1,8 @@
 const bcrypt = require("bcryptjs");
+const asyncHandler = require("express-async-handler");
 const { validationResult } = require("express-validator");
 const User = require("../models/user");
+
+exports.register = asyncHandler(async (req, res, next) => {
+  res.send("Register template");
+});
